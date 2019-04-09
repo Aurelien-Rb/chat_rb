8cfb0ad679c24dac1f2e6975804d31873a4606c2
Aurelien-Rb <aurelien.gilbert@stjosup.com>
2019-04-09 12:00:56 +0200

    Style

--- db/migrate/20190301144722_droproom_users.rb
+++
@@ -1,5 +0,0 @@
-class DroproomUsers < ActiveRecord::Migration[5.2]
-  def change
-  	 drop_table :room_users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_171638) do
+ActiveRecord::Schema.define(version: 2019_03_07_171537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_171638) do
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
     t.index ["room_id", "user_id"], name: "index_rooms_users_on_room_id_and_user_id"
-    t.index ["user_id", "room_id"], name: "index_rooms_users_on_user_id_and_room_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,8 +51,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_171638) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_"
-    t.integer "friend"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
